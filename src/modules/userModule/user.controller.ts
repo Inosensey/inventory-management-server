c340ec7ed8dbe60a506e3ab7a6d6d30ceaf062be
Philip Mathew <dingcong.bae@gmail.com>
2025-04-25 15:17:17 +0800

    User Controller updates: - Imported Body and Post from @nestjs/common. - Imported DTOs: CreateUserDTO, SelectUserDTO, and UserListResponseDto. - Imported esponse and userInfo from genericTypes. - Updated base route from user to users. - Added two endpoints: getUsers and createUser.

--- src/modules/userModule/user.controller.ts
+++ src/modules/userModule/user.controller.ts
@@ -1,15 +1,57 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { Types } from 'mongoose';
 
 // Service
 import { UserService } from './user.service';
 
-@Controller('user')
+// Dto
+import { CreateUserDTO, SelectUserDTO, UserListResponseDto } from './user.dto';
+
+// Types
+import { response, userInfo } from 'src/types/genericTypes';
+
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // Example endpoint
+  @Get()
+  async getUsers() {
+    const users = await this.userService.getUsers();
+    return plainToInstance(SelectUserDTO, users, {
+      excludeExtraneousValues: true,
+    });
+  }
+
   @Get(':id')
   getUserById(@Param('id') id: string) {
     return this.userService.getUserById(id);
+  }
+
+  @Post()
+  async createUser(@Body() user: CreateUserDTO) {
+    const result = await this.userService.createUser(user);
+
+    const userInfo: response<userInfo[]> = {
+      success: true,
+      data: [
+        {
+          id: result._id instanceof Types.ObjectId ? result._id.toString() : '',
+          roleId:
+            result.roleId instanceof Types.ObjectId
+              ? result.roleId.toString()
+              : '',
+          email: result.email,
+          username: result.username,
+          firstName: result.firstName,
+          lastName: result.lastName,
+        },
+      ],
+      message: '',
+    };
+
+    return plainToInstance(UserListResponseDto, userInfo, {
+      excludeExtraneousValues: true,
+    });
   }
 }
