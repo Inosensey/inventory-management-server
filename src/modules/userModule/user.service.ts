e38a1a76eba82c7a0c638e71e4c769c9aa14c65c
Philip Mathew <dingcong.bae@gmail.com>
2025-04-25 15:17:06 +0800

    User Service updates: - Imported required dependencies (Model, InjectModel, User, CreateUserDTO). - Injected UserModel in the constructor. - Added two new service methods: getUsers and createUser.

--- src/modules/userModule/user.service.ts
+++ src/modules/userModule/user.service.ts
@@ -1,8 +1,23 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { CreateUserDTO } from './user.dto';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+  async getUsers() {
+    return await this.userModel.find().exec();
+  }
+
   getUserById(id: string) {
     return { id, name: 'John Doe' };
+  }
+
+  async createUser(user: CreateUserDTO): Promise<User> {
+    const newUser = new this.userModel(user);
+    return await newUser.save();
   }
 }
