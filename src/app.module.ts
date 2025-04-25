66d0a50e1572241ff099400dc02358f3428b9c05
Philip Mathew <dingcong.bae@gmail.com>
2025-04-25 15:15:32 +0800

    Imported and applied the MongoDB config in pp.module.ts to establish database connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,41 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
+import mongoose from 'mongoose';
+import { mongooseConfig } from './config/mongoose.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // Modules
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '@modules/userModule/user.module';
 import { ProductModule } from '@modules/productModule/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: mongooseConfig,
+    }),
+    UserModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  onApplicationBootstrap() {
+    mongoose.connection.on('connected', () => {
+      console.log('✅ Connected to MongoDB');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('❌ MongoDB connection error:', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.warn('⚠️ MongoDB disconnected');
+    });
+  }
+}
