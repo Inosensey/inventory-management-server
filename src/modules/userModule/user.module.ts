f725c5bce3d2035646d68e1e067d84c2f41a284b
Inosensey <dingcong.bae@gmail.com>
2025-04-28 07:46:23 +0800

    Imported the IsEmailUniqueConstraint and IsUsernameUniqueConstraint from the mongodb-input.validator and add to the providers.

--- src/modules/userModule/user.module.ts
+++ src/modules/userModule/user.module.ts
@@ -3,10 +3,14 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './user.schema';
+import {
+  IsEmailUniqueConstraint,
+  IsUsernameUniqueConstraint,
+} from 'src/customValidator/mongodb-input.validator';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, IsUsernameUniqueConstraint, IsEmailUniqueConstraint],
 })
 export class UserModule {}
