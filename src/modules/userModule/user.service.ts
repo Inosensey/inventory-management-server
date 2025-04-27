5f39e1a5e0b95fea41b5d3e97ab5d4b10ef81c13
Inosensey <dingcong.bae@gmail.com>
2025-04-28 07:46:38 +0800

    UserService Updates: - Imported bcrypt. - Imported jwt. - Imported four exceptions called HttpException, Injectable, InternalServerErrorException, NotFoundException. - Imported UserCredentialsDTO DTO. - Updated the getUserById. - Renamed the createUser method to signUp and made adjustments to it. - Created three new methods called signIn, deleteUserByEmail, and isValueUnique.

--- src/modules/userModule/user.service.ts
+++ src/modules/userModule/user.service.ts
@@ -1,23 +1,101 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './user.schema';
-import { CreateUserDTO } from './user.dto';
+import { Users } from './user.schema';
+import { CreateUserDTO, UserCredentialsDTO } from './user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel('User') private userModel: Model<User>) {}
+  constructor(@InjectModel('User') private userModel: Model<Users>) {}
 
   async getUsers() {
     return await this.userModel.find().exec();
   }
 
-  getUserById(id: string) {
-    return { id, name: 'John Doe' };
+  async getUserById(id: string) {
+    try {
+      const user = await this.userModel.findOne({ _id: id }).lean();
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  async createUser(user: CreateUserDTO): Promise<User> {
-    const newUser = new this.userModel(user);
-    return await newUser.save();
+  async signUp(user: CreateUserDTO) {
+    try {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      const newUserInfo: CreateUserDTO = {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        password: hashedPassword,
+        roleId: user.roleId,
+      };
+      const newUser = new this.userModel(newUserInfo);
+      return await newUser.save();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async signIn(credentials: UserCredentialsDTO) {
+    try {
+      const user = await this.userModel
+        .findOne({ email: credentials.email })
+        .select('+password');
+
+      if (!user) {
+        throw new NotFoundException('Invalid Credentials!');
+      }
+
+      const passwordIsValid = await bcrypt.compare(
+        credentials.password,
+        user.password,
+      );
+
+      if (!passwordIsValid) {
+        throw new NotFoundException('Invalid Credentials!');
+      }
+
+      const token = jwt.sign(
+        { userId: user._id, roleId: user.roleId },
+        process.env.JWT_SECRET ||
+          '9c7708264b359ca23c76e30114cf405ec9c6c1c69230acbb1284a578cbe392a7',
+        { expiresIn: '1d' },
+      );
+
+      return { user, token };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteUserByEmail(email: string) {
+    try {
+      const result = await this.userModel.deleteOne({ email: email });
+      if (!result.acknowledged) {
+        throw new NotFoundException('User not found');
+      }
+      return true;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async isValueUnique(fieldName: keyof Users, value: string) {
+    const query = { [fieldName]: value };
+    const user = await this.userModel.findOne(query);
+    return !user;
   }
 }
