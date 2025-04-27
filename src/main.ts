9b8dc3a107055dd0c712024bd5fb381f26f7818f
Inosensey <dingcong.bae@gmail.com>
2025-04-28 07:44:35 +0800

    Main.ts Updates: - Imported the useContainer for custom class-validator. - Imported the ValidationPipe for DTO validations. - Imported the AllExceptionsFilter for the custom Exceptions. - Imported the cookieParser  for setting cookies.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './ExceptionFilter';
+import { AllExceptionsFilter } from './ExceptionFilter';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
