77b21a589de705c60b0f44f656b982d3128aded2
Philip Mathew <dingcong.bae@gmail.com>
2025-04-28 16:28:44 +0800

    ProductService Updates: - Injected the Products Schema to the constructor. - Created two new methods called createProduct and updateProduct.

--- src/modules/productModule/product.service.ts
+++ src/modules/productModule/product.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Products } from './product.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateProductDTO, UpdateProductDTO } from './product.dto';
 
 @Injectable()
 export class ProductService {
+  constructor(@InjectModel('Products') private productModel: Model<Products>) {}
   getProducts() {
     return [
       { id: 1, name: 'Product A', price: 100 },
       { id: 2, name: 'Product B', price: 200 },
     ];
+  }
+
+  async createProduct(product: CreateProductDTO) {
+    try {
+      const newProduct = new this.productModel(product);
+      return await newProduct.save();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async updateProduct(product: UpdateProductDTO) {
+    try {
+      const updatedProduct = await this.productModel.findByIdAndUpdate(
+        product.id,
+        product,
+      );
+      return updatedProduct;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
