ae734e77a3f6afb77da1946fa51ceb78b6cb922b
Philip Mathew <dingcong.bae@gmail.com>
2025-04-28 16:29:21 +0800

    ProductModule Updates: - Imported the AuthMiddleware. - Applied the AuthMiddleware to these paths products, products/add-product, products/update-product, and products/delete-product. Will be adding more later.

--- src/modules/productModule/product.module.ts
+++ src/modules/productModule/product.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { AuthMiddleware } from 'src/middleware/auth.middleware';
 
 @Module({
   imports: [],
   controllers: [ProductController],
   providers: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      { path: 'products', method: RequestMethod.GET },
+      { path: 'products/add-product', method: RequestMethod.POST },
+      {
+        path: 'products/update-product',
+        method: RequestMethod.PUT,
+      },
+      {
+        path: 'products/delete-product',
+        method: RequestMethod.DELETE,
+      },
+    );
+  }
+}
