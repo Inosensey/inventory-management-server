bac70eb1422fea9ff5446023428292aed84bcb24
Philip Mathew <dingcong.bae@gmail.com>
2025-04-28 16:29:03 +0800

    ProductController Updates: - Created three new methods called ddProduct, updateProduct, and deleteProduct. - Implemented UseGuards using the RoleGuard to the two paths /update-product and /delete-product.

--- src/modules/productModule/product.controller.ts
+++ src/modules/productModule/product.controller.ts
@@ -1,7 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+
+// Guards
+import { RoleGuard } from 'src/guards/auth.guard';
 
 // Services
 import { ProductService } from './product.service';
+import {
+  CreateProductDTO,
+  ProductResponseDto,
+  UpdateProductDTO,
+} from './product.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('products')
 export class ProductController {
@@ -10,5 +28,49 @@ export class ProductController {
   @Get()
   getProducts() {
     return this.productService.getProducts();
+  }
+
+  @Post('/add-product')
+  async addProduct(@Body() product: CreateProductDTO) {
+    const result = await this.productService.createProduct(product);
+
+    const response = plainToInstance(
+      ProductResponseDto,
+      {
+        success: true,
+        data: [result],
+        message: '',
+      },
+      {
+        excludeExtraneousValues: true,
+      },
+    );
+
+    return response;
+  }
+
+  @Put('/update-product')
+  @UseGuards(RoleGuard)
+  updateProduct(@Body() product: UpdateProductDTO) {
+    const result = this.productService.updateProduct(product);
+    const response = plainToInstance(
+      ProductResponseDto,
+      {
+        success: true,
+        data: [result],
+        message: '',
+      },
+      {
+        excludeExtraneousValues: true,
+      },
+    );
+    return response;
+  }
+
+  @Delete('/delete-product')
+  @UseGuards(RoleGuard)
+  deleteProduct(@Param('productId') productId: string) {
+    const test = `delete product ${productId}`;
+    return test;
   }
 }
