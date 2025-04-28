83322e5db100ecd14c8eee88c3bcdc1a588382a8
Philip Mathew <dingcong.bae@gmail.com>
2025-04-28 16:28:33 +0800

    ProductDTO Updates: - Renamed the ProductDTO to CreateProductDTO and made updates to it. - Created four new DTOs called UpdateProductDTO, categoryDTO, SelectProductDTO, and ProductResponseDto.

--- src/modules/productModule/product.dto.ts
+++ src/modules/productModule/product.dto.ts
@@ -1,18 +1,89 @@
-import { IsInt, IsString, IsOptional } from 'class-validator';
-export class ProductDTO {
-  @IsOptional()
-  @IsInt()
-  id?: number;
-
-  @IsString()
-  description: string;
-
-  @IsInt()
-  category: number;
-
+import { Expose, Transform, Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsInt,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+export class CreateProductDTO {
+  @IsNotEmpty()
   @IsString()
   name: string;
 
+  @IsNotEmpty()
+  @IsMongoId()
+  category: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
   @IsInt()
   price: number;
+}
+
+export class UpdateProductDTO {
+  @IsMongoId()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsMongoId()
+  category: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  price: number;
+}
+
+class categoryDTO {
+  @Expose()
+  categoryName: string;
+}
+
+export class SelectProductDTO {
+  @Expose()
+  @Transform(({ obj }: { obj: { _id: string } }) => obj._id.toString())
+  id: string;
+
+  @Expose()
+  name: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => categoryDTO)
+  @Expose()
+  category?: categoryDTO[];
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  description: string;
+}
+
+export class ProductResponseDto {
+  @Expose()
+  @IsBoolean()
+  success: boolean;
+
+  @Expose()
+  @ValidateNested({ each: true })
+  @Type(() => SelectProductDTO)
+  data: SelectProductDTO[];
+
+  @Expose()
+  @IsString()
+  message: string;
 }
