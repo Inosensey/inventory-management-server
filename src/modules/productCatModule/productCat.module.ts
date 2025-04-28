28bbb8f18a16d9d78690b474b6b9d4006a5f465c
Inosensey <dingcong.bae@gmail.com>
2025-04-29 06:36:54 +0800

    Fixed the typo file name from procutModule.ts to productModule.ts. Added the product-categories/bulk-add-categories path for auth checks.

---
+++ src/modules/productCatModule/productCat.module.ts
@@ -0,0 +1,32 @@
+import {
+  Module,
+  MiddlewareConsumer,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthMiddleware } from 'src/middleware/auth.middleware';
+import { product_categoriesSchema } from './productCat.schema';
+import { ProductCatService } from './productCat.service';
+import { ProductCatController } from './productCat.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'product_categories', schema: product_categoriesSchema },
+    ]),
+  ],
+  controllers: [ProductCatController],
+  providers: [ProductCatService],
+})
+export class ProductCatModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      { path: 'product-categories', method: RequestMethod.GET },
+      {
+        path: 'product-categories/bulk-add-categories',
+        method: RequestMethod.POST,
+      },
+    );
+  }
+}
