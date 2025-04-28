e23d95cbdae7993d01386ee518af0ed5e70eece6
Philip Mathew <dingcong.bae@gmail.com>
2025-04-28 16:27:24 +0800

    UserModule Updates: - Imported the AuthMiddleware. - Applied the AuthMiddleware to the users path. Will be adding more later.

--- src/modules/userModule/user.module.ts
+++ src/modules/userModule/user.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  MiddlewareConsumer,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,10 +12,17 @@ import {
   IsEmailUniqueConstraint,
   IsUsernameUniqueConstraint,
 } from 'src/customValidator/mongodb-input.validator';
+import { AuthMiddleware } from 'src/middleware/auth.middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UserController],
   providers: [UserService, IsUsernameUniqueConstraint, IsEmailUniqueConstraint],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
+}
