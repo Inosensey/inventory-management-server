3cfb9e53f243533dbe291d80176c66fd392854ac
Inosensey <dingcong.bae@gmail.com>
2025-04-29 06:37:59 +0800

    ProductService Updates: - Updated the logic of getProducts method. - Created another method called ulkCreateProducts. - Updated the logic of deleteProduct method. - Created another method called searchProduct.

--- src/modules/productModule/product.service.ts
+++ src/modules/productModule/product.service.ts
@@ -2,16 +2,42 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Products } from './product.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateProductDTO, UpdateProductDTO } from './product.dto';
+import {
+  CreateProductDTO,
+  SelectProductDTO,
+  UpdateProductDTO,
+} from './product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(@InjectModel('Products') private productModel: Model<Products>) {}
-  getProducts() {
-    return [
-      { id: 1, name: 'Product A', price: 100 },
-      { id: 2, name: 'Product B', price: 200 },
-    ];
+  async getProducts(currentPage?: string, setPageSize?: string) {
+    const page = currentPage ? parseInt(currentPage) : 1;
+    const pageSize = setPageSize ? parseInt(setPageSize) : 10;
+    console.log(pageSize);
+    const products = (await this.productModel
+      .aggregate([
+        {
+          $lookup: {
+            from: 'product_categories',
+            localField: 'category',
+            foreignField: '_id',
+            as: 'categoryInfo',
+          },
+        },
+      ])
+      .skip((page - 1) * pageSize)
+      .limit(pageSize)) as SelectProductDTO[];
+    return products;
+  }
+
+  async bulkCreateProducts(products: CreateProductDTO[]) {
+    try {
+      const buildAddProducts = await this.productModel.insertMany(products);
+      return buildAddProducts;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async createProduct(product: CreateProductDTO) {
@@ -30,6 +56,51 @@ export class ProductService {
         product,
       );
       return updatedProduct;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteProduct(id: string) {
+    try {
+      const deletedProduct = await this.productModel.findByIdAndDelete(id);
+      return deletedProduct;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async searchProduct(
+    search: string,
+    description: string,
+    currentPage?: string,
+    setPageSize?: string,
+  ) {
+    const page = currentPage ? parseInt(currentPage) : 1;
+    const pageSize = setPageSize ? parseInt(setPageSize) : 10;
+    try {
+      const products = (await this.productModel
+        .aggregate([
+          {
+            $match: {
+              $or: [
+                { name: { $regex: search, $options: 'i' } },
+                { description: { $regex: description, $options: 'i' } },
+              ],
+            },
+          },
+          {
+            $lookup: {
+              from: 'product_categories',
+              localField: 'category',
+              foreignField: '_id',
+              as: 'categoryInfo',
+            },
+          },
+        ])
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)) as SelectProductDTO[];
+      return products;
     } catch (error) {
       throw new InternalServerErrorException(error);
     }
