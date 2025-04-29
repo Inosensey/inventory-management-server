062160c6b0b0dfebdf894d51945fdbbb040991b0
Philip Mathew <dingcong.bae@gmail.com>
2025-04-29 14:13:31 +0800

    Remove the commented code.

--- src/customValidator/mongodb-input.validator.ts
+++ src/customValidator/mongodb-input.validator.ts
@@ -6,20 +6,7 @@ import {
   ValidatorConstraintInterface,
 } from 'class-validator';
 import { Injectable } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { Model } from 'mongoose';
-// import { Users } from '@modules/userModule/user.schema';
 import { UserService } from '@modules/userModule/user.service';
-
-// async function isValueUnique(
-//   userModel: Model<Users>,
-//   fieldName: keyof Users,
-//   value: string,
-// ) {
-//   const query = { [fieldName]: value };
-//   const user = await userModel.findOne(query);
-//   return !user;
-// }
 
 @ValidatorConstraint({ async: true })
 @Injectable()
@@ -27,7 +14,6 @@ export class IsEmailUniqueConstraint implements ValidatorConstraintInterface {
   constructor(private readonly userService: UserService) {}
 
   async validate(email: string) {
-    // return isValueUnique(this.userModel, 'email', email);
     return this.userService.isValueUnique('email', email);
   }
 
@@ -45,7 +31,6 @@ export class IsUsernameUniqueConstraint
 
   async validate(username: string) {
     return this.userService.isValueUnique('username', username);
-    // return isValueUnique(this.userModel, 'username', username);
   }
 
   defaultMessage(args: ValidationArguments) {
